430fe14bb3d3b83a5e87a324e23721ef5084e98a
elviac94 <elvi_ac94@hotmail.com>
2020-05-24 13:39:57 +0200

    add header and footer to project list component, and create projects JSON

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
 
 const ProjectsList = () => {
     return (
+        <>
+        <Header/>
         <ul className="projects__container">
             <h1> Hola, vamos a comprobar si has hecho bien el enrutamiento</h1>
         </ul>
+        <Footer/>
+        </>
     )
 }
 export default ProjectsList;

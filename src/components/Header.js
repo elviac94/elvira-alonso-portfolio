dc36b904835a064cbc0b699e063c41ec136e6f3f
elviac94 <elvi_ac94@hotmail.com>
2020-05-26 11:18:28 +0200

    header options

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,39 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+
+    const toggleHidden = (event) =>{
+        const option= event.currentTarget.className
+        const headerOptions = document.querySelectorAll('ul.header__list > li');
+        const home = document.querySelector('.header__list--home')
+        headerOptions.forEach(headerOption=>{
+            switch(headerOption.className){
+                case option:
+                headerOption.classList.toggle('hidden') && home.classList.remove('hidden');
+                break;
+                case home.classList.contains('header__list--home'):
+                headerOption.classList.add('hidden');
+                break;
+            }
+        })
+    }
+
+    // headerOption.className === option ?
+            //  headerOption.classList.toggle('hidden') && home.classList.remove('hidden')
+            //  : headerOption.classList.remove('hidden')
+
     return (
         <header className="header__container">
             <nav className="header__menu">
                 <ul className="header__list">
-                    <li className="header__list--about">About</li>
-                    <Link className="header__list--link" to='projects'>
-                    <li className="header__list--work">Work</li>
+                    <Link className="header__list--link" to='/'>
+                        <li className="header__list--home hidden" onClick={toggleHidden}>Home</li>
                     </Link>
-                    <li className="header__list--contact">Contact</li>
+                    <li className="header__list--about" onClick={toggleHidden}>About</li>
+                    <Link className="header__list--link" to='projects'>
+                        <li className="header__list--work" onClick={toggleHidden}>Work</li>
+                    </Link>
+                    <li className="header__list--contact" onClick={toggleHidden}>Contact</li>
                 </ul>
             </nav>
         </header>

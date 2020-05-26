dd3a1dd3b7ad74b7f52416daab6b0ae1c5b4ab4f
elviac94 <elvi_ac94@hotmail.com>
2020-05-26 16:13:40 +0200

    change condition header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,30 +3,25 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
 
-    const toggleHidden = (event) =>{
-        const option= event.currentTarget.className
+    const toggleHidden = (event) => {
+        const option = event.currentTarget.className
         const headerOptions = document.querySelectorAll('.header__list--link > li');
-        const home = document.querySelector('.header__list--home')
-        headerOptions.forEach(headerOption=>{
-          if(headerOption.className === option){
-              return( headerOption.parentElement.classList.toggle('hidden') && home.parentElement.classList.remove('hidden'))
-          }
-          if(headerOption.className === home.classList.contains('header__list--home')){
-              headerOption.classList.add('hidden')
-          }
+        //const home = document.querySelector('.header__list--home')
+        headerOptions.forEach(headerOption => {
+            if (headerOption.className === option) {
+                headerOption.parentElement.classList.add('hidden')
+            } else {
+                headerOption.parentElement.classList.remove('hidden')
+            }
         })
     }
-
-    // headerOption.className === option ?
-            //  headerOption.classList.toggle('hidden') && home.classList.remove('hidden')
-            //  : headerOption.classList.remove('hidden')
 
     return (
         <header className="header__container">
             <nav className="header__menu">
                 <ul className="header__list">
                     <Link className="header__list--link hidden" to='/'>
-                        <li className="header__list--home " onClick={toggleHidden}>Home</li>
+                        <li className="header__list--home" onClick={toggleHidden}>Home</li>
                     </Link>
                     <li className="header__list--about" onClick={toggleHidden}>About</li>
                     <Link className="header__list--link" to='projects'>

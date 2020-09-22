1ace6fc7ad7a40591fef867abd35088b3b5b4959
elviac94 <elvi_ac94@hotmail.com>
2020-09-22 17:48:53 +0200

    add button styles

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -15,21 +15,18 @@ const ProjectCard = (props) => {
     return (
         <div className="projectcard__container" id={id} onClick={flipCard}>
             <div className="picture__container">
-            <img src={source} alt={name}/>
+                <img src={source} alt={name} />
             </div>
-            
             <div className="front">
                 <div className="front__projects">
                     <h4 className="project__name">{name}</h4>
                     <div className="project__link--container">
-                        <a className="project__link" href={URL} target="blank"><i className="fas fa-code"></i></a>
-                        <a className="project__link" href={demo} target="blank"><i className="far fa-desktop"></i></a>
+                        <a className="project__link" href={URL} target="blank" title="enlace a código"><i className="fas fa-code"></i></a>
+                        <a className="project__link" href={demo} target="blank" title="ver página en funcionamiento"><i className="far fa-desktop"></i></a>
                     </div>
                 </div>
-
             </div>
             <div className="back">
-                {/* <h3 className="project__name">{name}</h3> */}
                 <ul className="project__description">{tecnologies.map(tecnology => <li key={tecnology}>{tecnology}</li>
                 )}</ul>
             </div>

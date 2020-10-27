8f247d71fcf3aa025e78228b6b27fe778940812a
elviac94 <elvi_ac94@hotmail.com>
2020-10-27 12:02:01 +0100

    add footer styles and change height of header and landing pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,12 +9,17 @@ const Footer = () => {
     return (
         <footer className="footer__container">
             <small className="footer__copyrigth">&copy; Elvira María Alonso, 2020</small>
-            <div className="rrss__container">
-                <a href="https://twitter.com/elviraalonsoc" target="blank" aria-label="Twitter"><img src={bird} alt="twitter-logo" title="Twitter"/></a>
-                <a href="https://github.com/elviac94" target="blank" aria-label="Github"><img src={github} alt="github-logo" title="Github"/></a>
-                <a href="https://linkedin.com/in/elviramariaalonsocervilla" target="blank" aria-label="Linkedin"><img src={linkedin} alt="linkedin-logo" title="Linkedin"/></a>
-                <a href="mailto:elviac94@gmail.com" target="blank" aria-label="Email"><img src={email} alt="email-logo" title="Email"/></a>
-            </div>
+            <ul className="rrss__container">
+                <li><a href="https://twitter.com/elviraalonsoc" target="blank" aria-label="Twitter">
+                    <img src={bird} alt="twitter-logo" title="Twitter" /></a></li>
+                <li><a href="https://github.com/elviac94" target="blank" aria-label="Github">
+                    <img src={github} alt="github-logo" title="Github" /></a></li>
+                <li><a href="https://linkedin.com/in/elviramariaalonsocervilla" target="blank" aria-label="Linkedin">
+                    <img src={linkedin} alt="linkedin-logo" title="Linkedin" /></a></li>
+                <li><a href="mailto:elviac94@gmail.com" target="blank" aria-label="Email">
+                    <img src={email} alt="email-logo" title="Email" /></a>
+                </li>
+            </ul>
         </footer>
     )
 }

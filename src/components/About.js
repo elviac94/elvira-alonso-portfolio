3501aec1d0225a8edb040a96588ccc72c994736c
elviac94 <elvi_ac94@hotmail.com>
2020-11-03 22:28:51 +0100

    add about structure and text

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,34 @@
 import React, { useEffect } from 'react';
+import ABOUT from '../data/ABOUT';
 
-const About =(props)=>{
+const About = (props) => {
 
     const { onShow } = props;
 
-    useEffect(()=>onShow('about'))
+    useEffect(() => onShow('about'))
 
-    return(
-        <div className="about__container">
-            <p>lorem impsum
-            </p>
-        </div>
+    const textAbout = ABOUT.text.split('. ', 3);
+    const technicalsSkills = ABOUT.skills.technicals;
+    const softsSkills = ABOUT.skills.soft;
+
+    return (
+        <section className="about__container">
+            {textAbout.map(text => <p>{text}.</p>)}
+            <div className="technicals__container">
+                <h3 className="technicals__title">technicals skills</h3>
+                {<ul className="technicals__list">
+                    {technicalsSkills.split(',')
+                        .map(skill => <li>{skill}</li>)}
+                </ul>}
+            </div>
+            <div className="softs__container">
+                <h3 className="softs__title">softs skills</h3>
+                {<ul className="softs__list">
+                    {softsSkills.split(',')
+                        .map(skill => <li>{skill}</li>)}
+                </ul>}
+            </div>
+        </section>
     )
 }
 export default About
---
+++ src/data/ABOUT.js
@@ -0,0 +1,8 @@
+const ABOUT = {
+    text: "I am frontend developer that enjoy solving code problems, and learning about new tecnologies. Before of this, I worked a time in customer assistant sector, this experience gave more skills to deal with people like  empathy and assertiveness. My hobbies are solve sudokus, play games and to read crime solving novels. If you want to know more information about me, you can found it in my resume.",
+    skills: {
+        technicals: "JavaScript, React, Redux, HTML, CSS, Gulp, SASS, PHP, MySQL, GIT",
+        soft: "Teamwork, Curiosity, Empathy, Colaborative, Resolutive, Assertiveness"
+    }
+}
+export default ABOUT;

c0b26a5ee4968dc4238111075c01be46510d9397
elviac94 <elvi_ac94@hotmail.com>
2020-11-04 21:42:40 +0100

    add about styles

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import ABOUT from '../data/ABOUT';
+import Resume from '../assets/resume/CV.pdf';
 
 const About = (props) => {
 
@@ -13,18 +14,21 @@ const About = (props) => {
 
     return (
         <section className="about__container">
-            {textAbout.map(text => <p>{text}.</p>)}
+            <div className="about__texts">
+                {textAbout.map(text => <p key={text}>{text}.</p>)}
+            </div>
+            <button type="button" className="resume__button"><a href={Resume} target="blank" title="resume" alt="resume" download={`Elvira-Alonso-CV${Resume}`}>Download CV</a></button>
             <div className="technicals__container">
                 <h3 className="technicals__title">technicals skills</h3>
                 {<ul className="technicals__list">
-                    {technicalsSkills.map(skill => <li><img src={skill.pic} alt={skill.tecnology}/></li>)}
+                    {technicalsSkills.map(skill => <li key={skill.tecnology}><img src={skill.pic} alt={skill.tecnology} title={skill.tecnology} /></li>)}
                 </ul>}
             </div>
             <div className="softs__container">
                 <h3 className="softs__title">softs skills</h3>
                 {<ul className="softs__list">
                     {softsSkills.split(',')
-                        .map(skill => <li>{skill}</li>)}
+                        .map(skill => <li key={skill}>{skill}</li>)}
                 </ul>}
             </div>
         </section>

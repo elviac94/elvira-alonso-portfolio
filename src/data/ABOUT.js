e9443a154f2a860f73d9f855c1ace89ce51fb263
elviac94 <elvi_ac94@hotmail.com>
2020-11-05 17:42:21 +0100

    add about's styles for tablet device

--- src/data/ABOUT.js
+++ src/data/ABOUT.js
@@ -10,7 +10,7 @@ import mysql from '../assets/icons/mysql.svg';
 import git from '../assets/icons/git.svg';
 
 const ABOUT = {
-    text: "I am frontend developer that enjoy solving code problems, and learning about new tecnologies. Before of this, I worked a time in customer assistant sector, this experience gave more skills to deal with people like  empathy and assertiveness. My hobbies are solve sudokus, play games and to read crime solving novels. If you want to know more information about me, you can found it in my resume.",
+    text: "I am frontend developer that enjoy solving code problems, and learning about new tecnologies. Before of this, I worked a time in customer service sector, this experience gave more skills to deal with people like  empathy and assertiveness. My hobbies are solve sudokus, play games and to read crime solving novels. If you want to know more information about me, you can found it in my resume.",
     skills: {
         technicals:[
             {

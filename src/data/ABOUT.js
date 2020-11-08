6bc36fb0d16b11214116c9dbef7f57825a95e472
elviac94 <elvi_ac94@hotmail.com>
2020-11-08 14:13:05 +0100

    change with of icons footer container and about me text

--- src/data/ABOUT.js
+++ src/data/ABOUT.js
@@ -10,7 +10,7 @@ import mysql from '../assets/icons/mysql.svg';
 import git from '../assets/icons/git.svg';
 
 const ABOUT = {
-    text: "I am frontend developer that enjoy solving code problems, and learning about new tecnologies. Before of this, I worked a time in customer service sector, this experience gave more skills to deal with people like  empathy and assertiveness. My hobbies are solve sudokus, play games and to read crime solving novels. If you want to know more information about me, you can found it in my resume.",
+    text: "I am a frontend developer that enjoy solving code problems, and learning about new technologies. Before of this, I worked a time the customer service sector, this experience gave me more skills to deal with people like  empathy and assertiveness. My hobbies are to solve sudokus, play games and to read crime novels",
     skills: {
         technicals:[
             {
